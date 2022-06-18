6c0a887e3f58b5712ccff03f6eebf66a268805f8
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-06-18 23:01:07 +0530

    inflated the float view on the screen

--- app/src/main/java/com/example/floatingview/FloatingViewService.java
+++ app/src/main/java/com/example/floatingview/FloatingViewService.java
@@ -3,12 +3,24 @@ package com.example.floatingview;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
 import java.security.Provider;
 
 public class FloatingViewService extends Service {
+
+    private View floatingWidget;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        floatingWidget = LayoutInflater.from(FloatingViewService.this)
+                .inflate(R.layout.floating_view_layout, null);
+    }
 
     @Nullable
     @Override
