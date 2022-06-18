ba8f397cc37a6cae897c258292194e3b315446ea
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-06-18 23:08:15 +0530

    created the window manager for the floating view

--- app/src/main/java/com/example/floatingview/FloatingViewService.java
+++ app/src/main/java/com/example/floatingview/FloatingViewService.java
@@ -2,9 +2,12 @@ package com.example.floatingview;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.IBinder;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 
@@ -13,6 +16,7 @@ import java.security.Provider;
 public class FloatingViewService extends Service {
 
     private View floatingWidget;
+    private WindowManager windowManager;
 
     @Override
     public void onCreate() {
@@ -20,6 +24,21 @@ public class FloatingViewService extends Service {
 
         floatingWidget = LayoutInflater.from(FloatingViewService.this)
                 .inflate(R.layout.floating_view_layout, null);
+
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        final WindowManager.LayoutParams parameters = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT
+        );
+
+        parameters.gravity = Gravity.TOP | Gravity.LEFT;
+        parameters.x = 200;
+        parameters.y = 200;
+
+        windowManager.addView(floatingWidget, parameters);
     }
 
     @Nullable
